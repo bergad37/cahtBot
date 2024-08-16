c089174dda95116e7c303d430acc3954b2efbb96
bergad37 <bergad37@gmail.com>
2024-08-16 15:30:00 +0200

    configure client and ser side

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import dotenv from 'dotenv';
+import path from "path";
 
 
 dotenv.config();
@@ -12,5 +13,32 @@ const io = new Server(httpServer, {});
 io.on("connection", (socket) => {});
 
 
-const port = process.env.PORT || 3000;
+app.use(express.static('src'));
+
+io.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected`);
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/src/index.html'));
+});
+io.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected`);
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+
+    socket.on('message', (data) => {
+        //sends the data to everyone except you.
+        // socket.broadcast.emit('response', data);
+
+        //sends the data to everyone connected to the server
+        socket.emit("response", data)
+    });
+})
+
+const port = process.env.PORT;
 httpServer.listen(port, () => { console.log(`Server listening to port ${port}`) });
---
+++ src/script.js
@@ -0,0 +1,26 @@
+const notify = document.querySelector('#notification');
+const message = document.querySelector('#message');
+const button = document.querySelector('button');
+const messageBar = document.querySelector('#message-bar');
+
+
+
+
+function printMessage(e) {
+    e.preventDefault();
+    console.log(message.value);
+}
+button.addEventListener('click', printMessage);
+const socket = io();
+
+
+socket.on('response', (data) => {
+    notify.textContent = data;
+    messageBar.style.backgroundColor = '#3F4E4F';
+    messageBar.style.height = '20vh';
+});
+
+function printMessage(e) {
+    e.preventDefault();
+    socket.emit('message', message.value);
+}
